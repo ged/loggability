5083a0452b9a456b22fbed34dc890162e657356f
Michael Granger <ged@FaerieMUD.org>
2015-02-27 19:16:33 -0800

    Use the current thread's object ID and not the Thread class's

--- spec/loggability/formatter_spec.rb
+++ spec/loggability/formatter_spec.rb
@@ -13,13 +13,14 @@ require 'loggability/formatter/default'
 describe Loggability::Formatter do
 
 	it "formats messages with the pattern it's constructed with" do
-		formatter = Loggability::Formatter.new( '[%5$s] %7$s' )
+		formatter = described_class.new( '[%5$s] %7$s' )
 		result = formatter.call( 'INFO', Time.at(1336286481), nil, 'Foom.' )
 		expect( result ).to match(/\[INFO\] Foom./i)
 	end
 
+
 	it "formats exceptions into useful messages" do
-		formatter = Loggability::Formatter.new( '[%5$s] %7$s' )
+		formatter = described_class.new( '[%5$s] %7$s' )
 		msg = nil
 
 		begin
@@ -31,11 +32,21 @@ describe Loggability::Formatter do
 		expect( msg ).to match(/\[INFO\] ArgumentError: invalid argument/i)
 	end
 
+
 	it "formats regular objects into useful messages" do
-		formatter = Loggability::Formatter.new( '[%5$s] %7$s' )
+		formatter = described_class.new( '[%5$s] %7$s' )
 		result = formatter.call( 'INFO', Time.at(1336286481), nil, Object.new )
 
 		expect( result ).to match(/\[INFO\] #<Object:0x[[:xdigit:]]+>/i)
+	end
+
+
+	it "includes the thread ID if logging from a thread other than the main thread" do
+		formatter = described_class.new( '%4$d' )
+		thr = Thread.new do
+			formatter.call( 'INFO', Time.now, nil, 'Foom.' )
+		end
+		expect( thr.value ).to eq( thr.object_id.to_s )
 	end
 
 end
